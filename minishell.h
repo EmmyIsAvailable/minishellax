6fa68c394520f269976ed3ec77db219db5b5daaf
U-DESKTOP-V5ULFUB\Emmeline <emmy.ruellan@orange.fr>
2022-02-23 14:11:22 +0100

    creation .h

---
+++ minishell.h
@@ -0,0 +1,30 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/*_INCLUDE_*/
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <curses.h>
+# include <term.h>
+# include <termios.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/ioctl.h>
+# include <errno.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <string.h>
+
+/*_FUNCTIONS_*/
+
+/*_main_*/
+
+#endif
