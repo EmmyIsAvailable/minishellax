d1644edf8e713fa87cd785eb032b8f9bf4d2d03c
chloedvx <chloe.daveux@gmail.com>
2022-04-08 14:28:49 +0200

    a== a regler

--- parsing/parse.c
+++ parsing/parse.c
@@ -18,33 +18,26 @@ int	find_token(char c)
 		return (REDIR_IN);
 	if (c == '>')
 		return (REDIR_OUT);
-//	if (c == '\'')
-//		return (SIMPLE_QUOTE);
-//	if (c == '"')
-//		return (DOUBLE_QUOTE);
+	if (c == '\'')
+		return (SIMPLE_QUOTE);
+	if (c == '\"')
+		return (DOUBLE_QUOTE);
 	if (c == '|')
 		return (PIPE);
+	if (c == '$')
+		return (DOLLAR_SIGN);
 	return (-1);
 }
 
-t_token	*fill_data(token_type token, int len, char *op)
+t_token	*ft_create_token(token_type token)
 {
 	t_token	*new_token;
-	int		i;
 
-	i = -1;
 	new_token = malloc(sizeof(t_token));
 	if (!new_token)
 		return (NULL);
 	new_token->token = token;
-	new_token->data = malloc(sizeof(char) * len + 1);
-	if (!new_token->data)
-		return (NULL);
-	while (op[++i] && i < len)
-		new_token->data[i] = op[i];
-	new_token->data[len] = '\0';
-	printf("data :%s\n", new_token->data);	
-	new_token->data_size = len;
+	new_token->data = NULL;
 	new_token->fd = 1;
 	new_token->next = NULL;
 	return (new_token);
@@ -70,85 +63,72 @@ char	*ft_search_env(char *params, t_data *data)
 	char	**envp;
 	int	i;
 
-	i = 0;
+	i = -1;
 	envp = data->envp;
-	while (envp[i])
+	while (envp[++i])
 	{
-		if (ft_strncmp(&envp[i][ft_name(params)], "=", 1) == 0) //ft_name va au dernier caractere non alphanum ou underscore
+		if (ft_strncmp(&envp[i][ft_name(params)], "=", 1) == 0)
 		{
 			if (ft_strncmp(envp[i], params, ft_name(params)) == 0)
 				return(&envp[i][ft_name(params) + 1]);
 		}
-		i++;
 	}
 	return (NULL);
 }
 
-char	*ft_dup(int i, int diff, char *str)
+char	*ft_dup(char *data, int i, int diff, char *str)
 {
 	char	*tmp;
 	int	j;
 
 	j = 0;
 	tmp = malloc(sizeof(char) * (i - diff + 1));
+	if (!tmp)
+		return (NULL);
 	while (j < i - diff)
 	{
 		tmp[j] = str[diff + j];
 		j++;
 	}
 	tmp[j] = '\0';
-	new_token->data = ft_strjoin(new_token->data, ft_dup(i, diff, str);
+	data = ft_strjoin(data, tmp);
 	free(tmp);
 	tmp = NULL;
+	return (data);
+}
 
+char	*ft_create_data(char *str, int i)
+{
+	char	*data;
+	int		j;
 
-
-
+	j = 0;
+	data = malloc(sizeof(char) * (i));
+	if (!data)
+		return (NULL);
+	while (str[++j] && j < i)
+		data[j - 1] = str[j];
+	data[i - 1] = '\0';
+	return (data);
 }
 
 t_token	*fill_data_quotes(token_type token, char *str, char op, t_data *data)
 {
 	t_token	*new_token;
 	int		i;
-	int		j;
 	int		diff;
-	char		*tmp;
 
 	i = 1;
-	j = 0;
 	diff = 1;
-	new_token = malloc(sizeof(t_token));
-	if (!new_token)
-		return (NULL);
-	new_token->token = token;
-	new_token->data = NULL;
+	new_token = ft_create_token(token);
 	while (str[i] != op)
 	{
 		if (str[i] == '$' && token == DOUBLE_QUOTE && ft_search_env(&str[i + 1], data))
 		{
-			j = 0;
-			if (i > 1 && !new_token->data)// tout ce qui est avant le $ est mis dans data 
-			{
-				new_token->data = malloc(sizeof(char) * (i));
-				if (!new_token->data)
-					return (NULL);
-				while (str[++j] && j < i)
-					new_token->data[j - 1] = str[j];
-				new_token->data[i - 1] = '\0';
-			}
-			else if (diff != i) //si on est sur un $ valable
-			{
-				tmp = 
-				while (j < i - diff)
-				{
-					tmp[j] = str[diff + j];
-					j++;
-				}
-				tmp[j] = '\0';
-				new_token->data = ft_strjoin(new_token->data, ft_dup(i, diff, str)
-				free(tmp);
-				tmp = NULL;
-			}
+			if (i > 1 && !new_token->data)
+				new_token->data = ft_create_data(str, i);
+			else if (diff != i)
+				new_token->data = ft_dup(new_token->data, i, diff, str);
 			new_token->data = ft_strjoin(new_token->data, (const char *)ft_search_env(&str[i + 1], data));
 			i += (1 + ft_name(&str[i + 1]));
 			diff = i;
@@ -156,38 +136,40 @@ t_token	*fill_data_quotes(token_type token, char *str, char op, t_data *data)
 		else
 			i++;
 	}
-	if (!new_token->data) //sans var env
-	{
-		new_token->data = malloc(sizeof(char) * (i));
-		if (!new_token->data)
-			return (NULL);
-		while (str[++j] && j < i)
-			new_token->data[j - 1] = str[j];
-		new_token->data[i - 1] = '\0';
-	}
-	else if (diff != i) //si il reste des trucs a la fin 
-	{
-		j = 0;
-		tmp = malloc(sizeof(char) * (i - diff + 1));
-		if (!tmp)
-			return (NULL);
-		while (j < i - diff)
-		{
-			tmp[j] = str[diff + j];
-			j++;
-		}
-		tmp[j] = '\0';
-		new_token->data = ft_strjoin(new_token->data, tmp);
-		free(tmp);
-	}
+	if (!new_token->data)
+		new_token->data = ft_create_data(str, i);
+	else if (diff != i)
+		new_token->data = ft_dup(new_token->data, i, diff, str);
 	new_token->data_size = i + 1;
-	printf("token size :%ld\n", new_token->data_size);
-	new_token->fd = 1;
-	new_token->next = NULL;
 	return (new_token);
 }
 
-t_token	*other_token(char *str, int io_here)
+t_token	*fill_data(token_type token, int len, char *op, t_data *data)
+{
+	t_token	*new_token;
+	int		i;
+
+	i = -1;
+	new_token = ft_create_token(token);
+	if (token == DOLLAR_SIGN && ft_search_env(&op[1], data)) //$ en dehors quotes
+	{
+			new_token->data = ft_strdup((const char *)ft_search_env(&op[1], data));
+			new_token->data_size = ft_name(&op[1]) + 1;
+			return (new_token);
+	}
+	if (token == DOLLAR_SIGN)
+		len = ft_name(&op[1]) + 1;
+	new_token->data = malloc(sizeof(char) * len + 1);
+	if (!new_token->data)
+		return (NULL);
+	while (op[++i] && i < len)
+		new_token->data[i] = op[i];
+	new_token->data[len] = '\0';
+	new_token->data_size = len;
+	return (new_token);
+}
+
+t_token	*other_token(char *str, int io_here, t_data *datas)
 {
 	int		i;
 	int		j;
@@ -195,42 +177,42 @@ t_token	*other_token(char *str, int io_here)
 
 	i = -1;
 	j = -1;
-	while (str[++j] && find_token(str[j]) == -1 && str[j] != ' ') //metttre tt isspae3
+	if (!str)
+		return (NULL);
+	while (str[++j] && find_token(str[j]) == -1 && (str[j] != '\t' || str[j] != '\v'
+		|| str[j] != '\n' || str[j] != '\r' || str[j] != '\f' || str[j] != 32))
 	{
-		if (!io_here && ft_isalnum(str[j]) == 0 && ft_strchr("_,/.-+$=*", (int)str[j]) == NULL) //liste arbitrairre de caracteres qui passent : peut etre tester tous les printable sauf isspace3
+		if (!io_here && ft_isalnum(str[j]) == 0 && ft_strchr("_,/.-+$=*", (int)str[j]) == NULL) //liste arbitrairre
 			break ;	
 	}
-	if (j == 0)
-		return (NULL);
 	data = malloc(sizeof(char) * j + 1);
-	if (data)
-	{
-		while (++i < j)
-			data[i] = str[i];
-		data[j] = '\0';
-		if (!io_here && data[0] == '$' && ft_strchr((const char *)data, '=') == NULL)
-			return (fill_data(DOLLAR_SIGN, j, data));
-		else if (!io_here && ft_strchr((const char *)data, '=') != NULL)
-			return (fill_data(ASSIGN, j, data));
-		return (fill_data(WORD, j, data));
-	}
+	if (!data)
+		return (NULL);
+	while (++i < j)
+	data[i] = str[i];
+	data[j] = '\0';
+	if (!io_here && ft_strchr((const char *)data, '=') != NULL)
+		return (fill_data(ASSIGN, j, data, datas));
+	return (fill_data(WORD, j, data, datas));
 	return (NULL);
 }
 
 t_token	*scan_token(char *str, int io_here, t_data *data)
 { 
 	if (ft_strncmp((const char *)str, "<<", 2) == 0)
-		return (fill_data(HEREDOC, 2, "<<"));
+		return (fill_data(HEREDOC, 2, "<<", data));
 	else if (ft_strncmp((const char *)str, ">>", 2) == 0)
-		return (fill_data(DOUBLE_GREATER, 2, ">>"));
+		return (fill_data(DOUBLE_GREATER, 2, ">>", data));
 	else if (ft_strncmp((const char *)str, "\"", 1) == 0)
 		return (fill_data_quotes(DOUBLE_QUOTE, str, '\"', data));
 	else if (ft_strncmp((const char *)str, "\'", 1) == 0)
-		return (fill_data_quotes(SIMPLE_QUOTE, str, '\'', data));// 0 interpretation de meta caracteres 
+		return (fill_data_quotes(SIMPLE_QUOTE, str, '\'', data));
+	else if (ft_strncmp((const char *)str, "$", 1) == 0)
+		return (fill_data(DOLLAR_SIGN, 0, &str[0], data));
 	else if (find_token(str[0]) != -1)
-		return (fill_data(find_token(str[0]), 1, &str[0]));
+		return (fill_data(find_token(str[0]), 1, &str[0], data));
 	else
-		return (other_token(str, io_here));
+		return (other_token(str, io_here, data));
 	return (NULL);
 }
 
@@ -257,7 +239,7 @@ int	ft_parse(char *str, t_token **head, t_data *data)
 			break;
 		if (space != i && space != 0)
 		{
-			tmp = fill_data(SPACE, 1, " ");
+			tmp = fill_data(SPACE, 1, " ", data);
 			ft_lst_add_back(head, tmp);
 		}
 		tmp = scan_token(&str[i], io_here_flag, data);
