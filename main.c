ddc3bb4dfba18a4443822229881213add8135a92
U-DESKTOP-V5ULFUB\Emmeline <emmy.ruellan@orange.fr>
2022-04-25 15:13:00 +0200

    main mieux

--- main.c
+++ main.c
@@ -47,22 +47,7 @@ int	init_envp(t_data *data, char **envp)
 	}
 	data->envp[i] = NULL;
 	data->shlvl = 1;
-//	data->prev = NULL;
-//	data->next = NULL;
 	return (0);
-}
-
-t_data	*add_shlvl_envp(t_data *data)
-{
-	t_data	*new;
-
-	new = NULL;
-	init_envp(new, data->envp);
-	new->shlvl = data->shlvl + 1;
-//	new->prev = data;
-//	new->next = NULL;
-//	data->next = new;
-	return (new);
 }
 
 int	ft_cmp_line(char *history, char *str)
@@ -87,40 +72,35 @@ int main(int ac, char **av, char **envp)
 	char	*history;
 	t_token	*head;
 	t_data	data;
-	t_data	*current;
 
 	(void)av;
 	history = NULL;
-	current = NULL;
 	if (ac != 1)
 	{
 		ft_putstr_fd("./minishell: too many arguments\n", 1);
 		return (1);
 	}
 	init_envp(&data, envp);
-	current = &data;
 	event_ctrl_c();
-	while (current->shlvl != -1)
+	while (data.shlvl != -1)
 	{
-		printf("%d\n", current->shlvl);
 		head = NULL;
 		history = readline("> ");
-		if ((current->shlvl == 1 && history == NULL) || (history && current->shlvl == 1 && ft_cmp_line(history, "exit") == 0))
+		if ((data.shlvl == 1 && history == NULL) || (history && data.shlvl == 1 && ft_cmp_line(history, "exit") == 0))
 		{
 			printf("exit\n");
 			break ;
 		}
 		else if (history && ft_cmp_line(history, getenv("_")) == 0)
-			current = add_shlvl_envp(current);
-		else if (current->shlvl > 1 && ((history && ft_cmp_line(history, "exit") == 0) || history == NULL))
+			data.shlvl++;
+		else if (data.shlvl > 1 && ((history && ft_cmp_line(history, "exit") == 0) || history == NULL))
 		{
-			//current = current->prev; // et il faut free l'envp qu'on quitte
-			//current.shlvl--;
+			data.shlvl--; // et il faut free l'envp qu'on quitte
 			history = NULL;
 			printf("exit\n");
 		}
 		if (history)
-			if (ft_parse(history, &head, current))
+			if (ft_parse(history, &head, &data))
 				return (1);
 		add_history(history);
 	}
