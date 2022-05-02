c088a6095f237101a410715ab31daae27f93edf5
chloedvx <chloe.daveux@gmail.com>
2022-05-02 18:11:24 +0200

    norminette parsing + opti

--- parsing/gestion_list_shlvl.c
+++ parsing/gestion_list_shlvl.c
@@ -47,27 +47,41 @@ void	ft_clear_shlvl(t_token **cmd, t_token **shlvl, t_token *cmp)
 	}
 }
 
-void	create_shlvl_list(t_token **cmd, t_data *data, t_token **shlvl)
+int	check_funct(int ex_or_unset, t_token *tmp, t_data *data)
+{
+	int	is_alpha;
+
+	is_alpha = ft_isalpha(tmp->data[0]);
+	if ((!ex_or_unset && tmp->data[ft_name(tmp->data)] == '=' && is_alpha)
+		|| (ft_search_env(tmp->data, data) && ex_or_unset == 1))
+		return (1);
+	return (0);
+}
+
+void	create_shlvl(int count, t_token **cmd, t_data *data, t_token **shlvl)
 {
 	t_token	*cmd_env;
+	t_token	*tmp;
 	int		ex_or_unset;
 
 	cmd_env = NULL;
-	ex_or_unset = ft_bool((*cmd)->data);
-	if ((ex_or_unset == 0 && ((*cmd)->next->data[ft_name((*cmd)->next->data)] == '='
-			&& ft_isalpha((*cmd)->next->data[0])))
-			|| (ft_search_env((*cmd)->next->data, data) && ex_or_unset == 1))
+	if (!count && (*cmd) && (*cmd)->next)
 	{
-		cmd_env = ft_create_token((*cmd)->next->token);
-		if (!cmd)
-			perror("malloc failed\n");
-		cmd_env->data = ft_strdup((*cmd)->next->data);
-		cmd_env->data_size = (*cmd)->next->data_size;
-		cmd_env->shlvl = data->shlvl;
-		cmd_env->cmd_env = ex_or_unset;
-		ft_clear_shlvl(cmd, shlvl, cmd_env);
-		if (*shlvl)
-			(*shlvl)->prev = cmd_env;
-		ft_lst_add(shlvl, cmd_env);
+		tmp = (*cmd)->next;
+		ex_or_unset = ft_bool((*cmd)->data);
+		if (check_funct(ex_or_unset, tmp, data))
+		{
+			cmd_env = ft_create_token((*cmd)->next->token);
+			if (!cmd)
+				perror("malloc failed\n");
+			cmd_env->data = ft_strdup((*cmd)->next->data);
+			cmd_env->data_size = (*cmd)->next->data_size;
+			cmd_env->shlvl = data->shlvl;
+			cmd_env->cmd_env = ex_or_unset;
+			ft_clear_shlvl(cmd, shlvl, cmd_env);
+			if (*shlvl)
+				(*shlvl)->prev = cmd_env;
+			ft_lst_add(shlvl, cmd_env);
+		}
 	}
 }
