915d017e89225c8b07b0814821599923dbbe5a68
Chloe DAVEUX <cdaveux@e1r8p4.42.fr>
2022-05-30 17:30:26 +0200

    parse quotes a la norme

--- parsing/parse_quotes.c
+++ parsing/parse_quotes.c
@@ -6,7 +6,7 @@
 /*   By: cdaveux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/22 11:08:38 by cdaveux           #+#    #+#             */
-/*   Updated: 2022/05/30 16:30:05 by cdaveux          ###   ########.fr       */
+/*   Updated: 2022/05/30 16:35:47 by cdaveux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,8 @@ int	dollar_in_quotes(t_token **new_token, char *str, char op, t_data *data)
 		{
 			if (!no_data(&(*new_token), i, str) && diff != i)
 				(*new_token)->data = ft_dup((*new_token)->data, i, diff, str);
-			(*new_token)->data = join_elems((*new_token)->data, ft_search_env(&str[i + 1], data));
+			(*new_token)->data = join_elems((*new_token)->data,
+					ft_search_env(&str[i + 1], data));
 			i += (1 + ft_name(&str[i + 1]));
 			diff = i;
 		}
